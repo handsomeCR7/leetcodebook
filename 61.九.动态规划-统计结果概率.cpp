db3ec6d269c5b42b1aeddb5e38d4ae5954704cbe
handsomeCR7 <270898831@qq.com>
2024-04-17 19:42:13 +0800

    动态规划

---
+++ 61.九.动态规划-统计结果概率.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<double> statisticsProbability(int num) {
+        // 从一开始到最后产生的所有数的个数，最后要舍弃前面num个数
+        vector<double> res(6, 1.0 / 6.0);
+        // 掷骰子
+        for (int i = 2; i <= num; i++) {
+            // 暂存此轮结果，以防改变原数据
+            vector<double> dp(5 * i + 1, 0);
+            // 以上一轮筛出的为底
+            for (int j = 0; j < res.size(); j++) {
+                for (int k = 0; k < 6; k++) {
+                    dp[j + k] += res[j] * 1.0 / 6.0;
+                }
+            }
+            res = dp;
+        }
+        return res;
+    }
+};
+
+int main() {
+    int num = 3;
+    Solution s;
+    vector<double> res = s.statisticsProbability(num);
+    for (auto res1 : res) {
+        cout << res1 << endl;
+    }
+    system("pause");
+    return 0;
+}
---
+++ 62.九.动态规划-模糊搜索验证.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool articleMatch(string s, string p) {
+        s = '0' + s;
+        p = '0' + p;
+        vector<vector<bool>> dp(s.size(), vector<bool>(p.size()));
+        dp[0][0] = 1;
+        for (int i = 2; i < p.size(); i += 2) {
+            if (dp[0][i - 2] == 1 && p[i] == '*') {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 1; i < s.size(); i++) {
+            for (int j = 1; j < p.size(); j++) {
+                if (p[j] == '.' && dp[i - 1][j - 1] == 1) {
+                    dp[i][j] = 1;
+                }
+                if (p[j] == s[i] && dp[i - 1][j - 1]) {
+                    dp[i][j] = 1;
+                }
+                if (p[j] == '*') {
+                    // x* = ''
+                    if (dp[i][j - 2] == 1) {
+                        dp[i][j] = 1;
+                    }
+                    // x* = 'x'或'xx'或'xxx'等等
+                    // *的每一列x都是递增的：如第一列是''，第二列是'x'，第三列是'xx'
+                    // 上一轮添加的x如果没有匹配上，那后面再添x就没意义了
+                    // 如果上一轮添加的x匹配上了，看本次如果再添加一次x，将s[i]和p[j-1]进行比较
+                    if (dp[i - 1][j] == 1 && s[i] == p[j - 1]) {
+                        dp[i][j] = 1;
+                    }
+                    // 特别地
+                    // 如果*前面是.，那这个*也是万能的了
+                    // 如果上一轮添加的x已经匹配上了，那本轮的*和s[i]直接匹配成功
+                    if (p[j - 1] == '.' && dp[i - 1][j] == 1) {
+                        dp[i][j] = 1;
+                    }
+                }
+            }
+        }
+        return dp[s.size() - 1][p.size() - 1];
+    }
+};
+
+int main() {
+    string s = "aaa";
+    string p = ".*";
+    Solution res;
+    cout << res.articleMatch(s, p) << endl;
+    system("pause");
+    return 0;
+}
